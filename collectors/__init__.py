9bd6b5a5e56280f1c8e39f0269006b808efcb674
jasonab74-ctrl <jasonab74@gmail.com>
2025-10-09 16:23:15 -0700

    Create __init__.py

---
+++ collectors/__init__.py
@@ -0,0 +1,3 @@
+# collectors/__init__.py
+# This package initializer lets you do: from collectors import youtube, openalex, etc.
+# No runtime code needed here; modules are discovered in main pipeline.
